6fed34359b79a30b623fb2c05a80ba3ba0510be4
iang12 <ianwandersong12@gmail.com>
2019-10-28 15:31:56 -0300

    alteração do modelo de mensagens

--- lib/models/model_message.dart
+++ lib/models/model_message.dart
@@ -1,64 +1,64 @@
-class Mensagens{
+class Mensagem{
   final String nome;
   final String mensagem;
   final String horario;
   final String imageUrl;
 
-  Mensagens({this.nome,this.mensagem,this.horario,this.imageUrl});
+  Mensagem({this.nome,this.mensagem,this.horario,this.imageUrl});
 }
 
-List<Mensagens> listaMensagens = [
-  new Mensagens(
+List<Mensagem> listaMensagens = [
+  new Mensagem(
     nome: "Marcos Oliveira",
     mensagem: "cara e a reunião? é hoje",
     horario: "12:30",
     imageUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "Maria Santos",
     mensagem: "Bom Dia.. Mais Tarde vou ai.",
     horario: "12:30",
     imageUrl: "https://images.pexels.com/photos/1236701/pexels-photo-1236701.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "Gabriela",
     mensagem: "kkkkkkkk",
     horario: "09:15",
     imageUrl: "https://images.pexels.com/photos/756453/pexels-photo-756453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "João Rt",
     mensagem: "ok,so me fala depois",
     horario: "12:30",
     imageUrl: "https://images.pexels.com/photos/684387/pexels-photo-684387.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "Uespi - Bloco 6 ",
     mensagem: "+55 86 994324465: Amanhã tem Aula?",
     horario: "09:20",
     imageUrl: "https://pbs.twimg.com/profile_images/1055072072546861057/GbhMNJSa.jpg"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "Flutter PHB",
     mensagem: "Flutter é sensacional mano:)",
     horario: "15:46",
     imageUrl: "https://static.techspot.com/images2/news/bigimage/2018/02/2018-02-28-image-6.jpg"
   ),
-  new Mensagens(
+  new Mensagem(
     nome: "Carla Souza",
     mensagem: "quinta-feira não tem aula?",
     horario: "19:30",
     imageUrl: "https://images.pexels.com/photos/1122868/pexels-photo-1122868.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
   
-  new Mensagens(
+  new Mensagem(
     nome: "Jaqueline ",
     mensagem: "olá..Bom Dia",
     horario: "09:20",
     imageUrl: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
   ),
 
-  new Mensagens(
+  new Mensagem(
     nome: "Ricardo ",
     mensagem: "olá..Bom Dia",
     horario: "09:20",
